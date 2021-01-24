1cd8a7958a5a753de0f875a70dafb7ce4618e24e
DKlaver <dklaver15@gmail.com>
2021-01-24 17:20:15 -0600

    Changed if the if statement when the program isn't able to find a nameto person.length < 1.  The original condition would still let errors through

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function mainMenu(person, people){
 
   /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
 
-  if(!person){
+  if(person.length < 1) {
     alert("Could not find that individual.");
     return app(people); // restart
   }
